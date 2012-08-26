0b989656ba8388e7e5e34261388f157c09374ef8
Wakaba <wakaba@lemon>
2012-08-26 22:24:55 +0900

    Fixed libpath

--- t/pmbp/install-module.sh
+++ t/pmbp/install-module.sh
@@ -3,10 +3,10 @@ echo "1..1"
 basedir=`dirname $0`/../..
 pmbp=$basedir/bin/pmbp.pl
 tempdir=`perl -MFile::Temp=tempdir -e 'print tempdir'`/testapp
-perl $pmbp \
-  --root-dir-name="$tempdir" \
-  --install-module=$1
-PERL5LIB="`perl $pmbp --print-libs`" perl -M$1 \
+perl $pmbp --root-dir-name="$tempdir" --install-module=$1
+libs=`perl $pmbp --root-dir-name="$tempdir" --print-libs`
+echo PERL5LIB=$libs
+PERL5LIB="$libs" perl -M$1 \
   -e '$'$1'::VERSION or die "No VERSION"' && \
   echo "ok 1" && \
   rm -fr $tempdir
