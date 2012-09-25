42dad7a0be5e14218cb42eb15ffc975212aecd16
Wakaba <wakaba@lemon>
2012-09-25 19:20:14 +0900

    variations

--- bin/pmbp.pl
+++ bin/pmbp.pl
@@ -479,8 +479,8 @@ sub cpanm ($$) {
               if $module_name;
         } elsif ($log =~ /^Can\'t call method "load_all_extensions" on an undefined value at inc\/Module\/Install.pm /m) {
           $remove_inc = 1;
-        } elsif ($log =~ /^Module::Build version \S+ required--this is only version \S+/m) {
-          push @required_install, PMBP::Module->new_from_package ('Module::Build');
+        } elsif ($log =~ /^(\S+) version \S+ required--this is only version \S+/m) {
+          push @required_install, PMBP::Module->new_from_package ($1);
         }
         $failed = 1;
       } elsif (/^! Couldn\'t find module or a distribution Date::Parse \(/) {
