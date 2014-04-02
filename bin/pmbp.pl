22dcebd4ad712bc552d413bd13cda7b688d7a09d
Wakaba <wakaba@suikawiki.org>
2014-04-03 00:31:33 +0900

    ~user in path

--- bin/pmbp.pl
+++ bin/pmbp.pl
@@ -910,6 +910,8 @@ sub install_perl ($) {
                   if ($_[0] =~ m{^  tail -f (.+?/perlbrew/build.perl-.+?\.log)}) {
                     $log_file_name = $1;
                     $log_file_name =~ s{^~/}{$ENV{HOME}/} if defined $ENV{HOME};
+                    $log_file_name = (glob $log_file_name) || $log_file_name
+                        if $log_file_name =~ m{^~};
                     return 0;
                   } elsif ($_[0] =~ /^It is possible that the compressed file\(s\) have become corrupted/) {
                     remove_tree "$RootDirName/local/perlbrew/dists";
