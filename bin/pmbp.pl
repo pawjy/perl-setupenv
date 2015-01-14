7f64e31331cff6091f57d766a642fa1e6e01e74a
Wakaba <wakaba@suikawiki.org>
2015-01-14 15:30:18 +0900

    revert

--- bin/pmbp.pl
+++ bin/pmbp.pl
@@ -2347,14 +2347,14 @@ PMBP_ORIG_PATH="`perl -e '%s'`" PATH="%s" PERL5LIB="\$libpaths" LD_LIBRARY_PATH=
         ($root_path eq '.' ? '`dirname $0`' : '`dirname $0`/'._quote_dq $root_path),
         _quote_dq +File::Spec->abs2rel (get_relative_libs_txt_file_name ($perl_version), $RootDirName),
         _quote_dq '${PMBP_ORIG_PATH:-PATH}',
-        _quote_dq "\$rootpath/$pm_path:\$rootpath/$perl_path:" . '${PMBP_ORIG_PATH:-$PATH}',
+        _quote_dq "\$rootpath/$pm_path:\$rootpath/$perl_path:" . '$PATH',
         _quote_dq '$rootpath/' . $lib_path . ':$LD_LIBRARY_PATH',
         (defined $command ? '"' . $command . '" ' : '') .
         (defined $arg ? '"' . $arg . '" ' : '');
   } else {
     printf $file qq{\#!/bin/sh\nPMBP_ORIG_PATH="%s" PATH="%s" PERL5LIB="`cat %s 2> /dev/null`" LD_LIBRARY_PATH="%s" exec %s"\$\@"\n},
         _quote_dq '${PMBP_ORIG_PATH:-PATH}',
-        _quote_dq "$pm_path:$perl_path:" . '${PMBP_ORIG_PATH:-$PATH}',
+        _quote_dq "$pm_path:$perl_path:" . '$PATH',
         _quote_dq get_libs_txt_file_name ($perl_version),
         _quote_dq $lib_path . ':$LD_LIBRARY_PATH',
         (defined $command ? '"' . $command . '" ' : '') .
