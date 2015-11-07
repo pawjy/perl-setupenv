aa9c934c9da303b2287a91bfda39289181a712f7
Wakaba <wakaba@suikawiki.org>
2015-11-07 17:57:17 +0900

    .EXE

--- bin/pmbp.pl
+++ bin/pmbp.pl
@@ -974,7 +974,7 @@ sub use_perl_core_module ($) {
             envs => {defined $perl_version ? (PATH => get_env_path ($perl_version)) : ()},
             discard_stderr => 1,
             onoutput => sub { $output = $_[0]; 3 }) {
-      if (defined $output and $output =~ m{^(\S*\Q$command\E)$}) {
+      if (defined $output and $output =~ m{^(\S*\Q$command\E(?:\.[A-Za-z0-9]+|))$}) {
         return $1;
       }
     }
