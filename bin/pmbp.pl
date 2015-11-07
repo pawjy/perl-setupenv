f94c49f9d53ae11bba0b8827d7a4d6ab400b6139
Wakaba <wakaba@suikawiki.org>
2015-11-07 20:36:39 +0900

    Retry sniffing

--- bin/pmbp.pl
+++ bin/pmbp.pl
@@ -1934,7 +1934,7 @@ sub cpanm ($$) {
         }
       }
       if ($level == 1 and
-          ($log =~ /! (?:Installing|Configuring) (\S+) failed\. .*?See (.+?) for details\./m or
+          ($log =~ /! (?:Installing|Configuring) (\S+) failed\. See (.+?) for details\./m or
            $log =~ /! Configure failed for (\S+). See (.+?) for details\.$/m)) {
         my $log = copy_log_file $2 => $1;
         $scan_errors->($level + 1, $log);
@@ -1946,6 +1946,11 @@ sub cpanm ($$) {
         }
 
         $failed = 1;
+      }
+      if ($level == 1 and
+          $log =~ /! Can't configure the distribution. You probably need to have 'make'. See (.+?) for details./m) {
+        my $log = copy_log_file $1 => 'cpanm';
+        $scan_errors->($level + 1, $log);
       }
       if ($log =~ m{^(\S+) \S+ is required to configure this module; please install it or upgrade your CPAN/CPANPLUS shell.}m) {
         push @required_install, PMBP::Module->new_from_package ($1);
