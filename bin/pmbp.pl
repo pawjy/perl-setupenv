7522d93acbc00fbca0fcf59d13cfbdd818e2779a
Wakaba <wakaba@suikawiki.org>
2017-10-05 22:57:09 +0900

    Merge two variants of shortcut shell script generations as far as possible

--- bin/pmbp.pl
+++ bin/pmbp.pl
@@ -2963,36 +2963,40 @@ sub create_perl_command_shortcut ($$$;%) {
   my $common_bin_path = resolve_path "local/common/bin", $RootDirName;
   my $common_lib_path = resolve_path "local/common/lib", $RootDirName;
   open my $file, '>', $file_name or info_die "$0: $file_name: $!";
+  my $paths;
   if ($args{relocatable}) {
     my $file_path = resolve_path '..', $file_name;
     my $root_path = File::Spec->abs2rel ($RootDirName, $file_path);
     $command = File::Spec->abs2rel ($command, $RootDirName)
         if defined $command and $command =~ m{/};
-    $pm_path = File::Spec->abs2rel ($pm_path);
-    $perl_path = File::Spec->abs2rel ($perl_path);
-    $lib_path = File::Spec->abs2rel ($lib_path);
-    $common_bin_path = File::Spec->abs2rel ($common_bin_path);
-    $common_lib_path = File::Spec->abs2rel ($common_lib_path);
-    printf $file qq{\#!/bin/bash
+    for ($pm_path, $perl_path, $lib_path, $common_bin_path, $common_lib_path) {
+      $_ = File::Spec->abs2rel ($_);
+      $_ = '$rootpath/' . $_;
+    }
+    $paths = sprintf q{
 rootpath="\$(cd %s && pwd)"
 libpaths=`cat \$rootpath/%s 2> /dev/null | perl -MCwd=abs_path -e '\\\$p = abs_path shift; local \\\$/ = undef; print join q{:}, map { \\\$p . q{/} . \\\$_ } split /:/, <>' "\$rootpath"`
-PMBP_ORIG_PATH="%s" PATH="%s" PERL5LIB="\$libpaths" LD_LIBRARY_PATH="%s" exec %s"\$\@"\n},
+    },
         ($root_path eq '.' ? '`dirname $0`' : '`dirname $0`/'._quote_dq $root_path),
         _quote_dq +File::Spec->abs2rel (get_relative_libs_txt_file_name ($perl_version), $RootDirName),
-        _quote_dq '${PMBP_ORIG_PATH:-$PATH}',
-        _quote_dq "\$rootpath/$pm_path:\$rootpath/$perl_path:\$rootpath/$common_bin_path:" . '${PMBP_ORIG_PATH:-$PATH}',
-        _quote_dq '$rootpath/' . $lib_path . ':$rootpath/' . $common_lib_path . ':$LD_LIBRARY_PATH',
-        (defined $command ? '"' . $command . '" ' : '') .
-        (defined $arg ? '"' . $arg . '" ' : '');
+    ;
   } else {
-    printf $file qq{\#!/bin/bash\nPMBP_ORIG_PATH="%s" PATH="%s" PERL5LIB="`cat %s 2> /dev/null`" LD_LIBRARY_PATH="%s" exec %s"\$\@"\n},
-        _quote_dq '${PMBP_ORIG_PATH:-$PATH}',
-        _quote_dq "$pm_path:$perl_path:$common_bin_path:" . '${PMBP_ORIG_PATH:-$PATH}',
+    $paths = sprintf q{
+libpaths=`cat %s 2> /dev/null`
+    },
         _quote_dq get_libs_txt_file_name ($perl_version),
-        _quote_dq $lib_path . ':' . $common_lib_path . ':$LD_LIBRARY_PATH',
-        (defined $command ? '"' . $command . '" ' : '') .
-        (defined $arg ? '"' . $arg . '" ' : '');
+    ;
   }
+  printf $file qq{\#!/bin/bash
+%s
+PMBP_ORIG_PATH="%s" PATH="%s" PERL5LIB="\$libpaths" LD_LIBRARY_PATH="%s" exec %s"\$\@"
+},
+      $paths,
+      _quote_dq '${PMBP_ORIG_PATH:-$PATH}',
+      _quote_dq "$pm_path:$perl_path:$common_bin_path:" . '${PMBP_ORIG_PATH:-$PATH}',
+      _quote_dq $lib_path . ':' . $common_lib_path . ':$LD_LIBRARY_PATH',
+      (defined $command ? '"' . $command . '" ' : '') .
+      (defined $arg ? '"' . $arg . '" ' : '');
   close $file;
   chmod 0755, $file_name or info_die "$0: $file_name: $!";
 } # create_perl_command_shortcut
