65f29ddd7afa89d827645d7f70657a0bbe31b538
Wakaba <wakaba@suikawiki.org>
2017-12-30 17:57:18 +0900

    Try to resolve Moose-based modules' version conflictions

--- bin/pmbp.pl
+++ bin/pmbp.pl
@@ -2301,6 +2301,10 @@ sub cpanm ($$) {
       if ($log =~ /^Can\'t call method "load_all_extensions" on an undefined value at inc\/Module\/Install.pm /m) {
         $remove_inc = 1;
       }
+      if ($log =~ m{Invalid version format \(version required\) at .+/Module/Runtime.pm line }m) {
+        ## Moose version inconsistency
+        push @required_install, PMBP::Module->new_from_package ('Moose');
+      }
       if ($log =~ /^(\S+) version \S+ required--this is only version \S+/m) {
         push @required_install, PMBP::Module->new_from_package ($1);
       }
