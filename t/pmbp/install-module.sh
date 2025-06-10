0882b74b38a8de3a26aa81a0aabc0408ba7e81bf
Wakaba <wakaba@suikawiki.org>
2025-06-10 18:33:09 +0900

    Updated tests to support .so path finding

--- t/pmbp/install-module.sh
+++ t/pmbp/install-module.sh
@@ -4,10 +4,11 @@ basedir=`dirname $0`/../..
 pmbp=$basedir/bin/pmbp.pl
 tempdir=`perl -MFile::Temp=tempdir -e 'print tempdir'`/testapp
 perl $pmbp --root-dir-name="$tempdir" $pmbp_pre_commands --install-module=$1 \
+     --create-perl-command-shortcut=perl \
   || echo "not ok 1"
 libs=`perl $pmbp --root-dir-name="$tempdir" --print-libs`
 #echo PERL5LIB=$libs
-(PERL5LIB="$libs" perl -M$1 \
+(./perl -M$1 \
   -e '$'$1'::VERSION or die "No VERSION"' && \
   echo "ok 1" && \
   rm -fr $tempdir) || echo "not ok 1"
